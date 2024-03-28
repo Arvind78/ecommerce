63c4a9717f65f3c87c439889b379ca00a83f958f
Arvind <av894883@gmail.com>
2024-03-28 15:37:59 -0700

    push

--- client/shopnow/vite.config.js
+++ client/shopnow/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://shopnow-073b.onrender.com',
-        changeOrigin: true,
+        // changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Strip '/api' from request path
       },
     },
